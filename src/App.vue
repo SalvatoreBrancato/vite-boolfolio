fc5bc84a57d04c4e7cee29b0c9394276281bd9e9
Salvatore Brancato <122605000+SalvatoreBrancato@users.noreply.github.com>
2023-06-28 16:22:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import MainComp from './components/MainComp.vue';
+import {store} from './store'
+
+
+
+export default{
+  name: "App",
+  components:{
+    MainComp
+},
+data(){
+  return{
+    store
+  }
+}
+}
+</script>
+
+<template>
+  <MainComp/>
+</template>
+
+<style lang="scss">
+@use "./style/main.scss";
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+  name: "MainComp",
+  components:{
+    
+  }
+}
+</script>
+
+<template>
+    <h1 class="animate__animated animate__bounce">Ciaoooooo</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
